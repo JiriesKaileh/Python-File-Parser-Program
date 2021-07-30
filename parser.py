6508d8cc0756bc138fc6288bc877eb3571e041d1
SKY FAB <skyfab@studentdomain.smccd.net>
2021-07-30 14:01:41 -0700

    cleaned up and commented the code

--- parser.py
+++ parser.py
@@ -1,86 +1,101 @@
-import os
-import statistics
-import matplotlib.pyplot as plt
-import numpy as np
+import os                                #for accessing and iterating through directories
+import statistics                        #for calculating standard deviation
+import matplotlib.pyplot as plt          #for generating histogram
+import numpy as np                       #for generating histogram
 
 
-anodeDir = 'C:\\Users\\skyfab\\Documents\\Waveforms\\Data\\anode'
-pmtDir = 'C:\\Users\\skyfab\\Documents\\Waveforms\\Data\\pmt'
 
-anodeTimes = []     #store timestamps for anode files
-pmtTimes = []       #store timestamps for pmt files
+
+
+
+anodeDir = 'C:\\Users\\skyfab\\Documents\\Waveforms\\Data\\anode'  #folder path to .csv acquisitions for AD1
+pmtDir = 'C:\\Users\\skyfab\\Documents\\Waveforms\\Data\\pmt'      #folder path to .csv acquisitions for AD2
+
+anodeTimes = []                                                    #list in which timestamps for anodeTimes is stored
+pmtTimes = []                                                      #list in which timestamps for pmtTimes is stored
+
+
+
 
 
 
 for filename in os.listdir(anodeDir):
-    #print(os.path.join(anodeDir, filename))       #prints the name of the file in the directory
-    
-    name = os.path.join(anodeDir, filename)
-    index = len(name) - 7 
+    name = os.path.join(anodeDir, filename)                        #store the name of the file in 'name'
+    index = len(name) - 5                                          #len(name) is the number of characters in name, and len(name) - 5 is ones place of the milliseconds in the timestamp 
+                                                                   #example: name = 'C:\Users\skyfab\Documents\Waveforms\Data\anode\anode12.25.43.116.csv' 
+                                                                   #the character name[index] corresponds to 6. We use the function int() to cast the character to an integer value
   
-    timeStampVal = int(name[index - 6]) * 600000 + int(name[index - 5]) * 60000 + int(name[index - 3]) * 10000 + int(name[index - 2]) * 1000 + int(name[index]) * 100 + int(name[index + 1]) * 10 + int(name[index + 2]) #taking the last three number in the timestamp (milliseconds) and adding it to anodeTimes
+    timeStampVal = int(name[index - 8]) * 600000 + int(name[index - 7]) * 60000 + int(name[index - 5]) * 10000 + int(name[index - 4]) * 1000 + int(name[index - 2]) * 100 + int(name[index - 1]) * 10 + int(name[index]) #converting the timestamp to milliseconds
 
-    #this method may be inaccurate when timing things near a change in hour
-
-    anodeTimes.append(timeStampVal) 
-     
-    
-    #print('\n')
-
-    
+    anodeTimes.append(timeStampVal)                                 #add timestamp value to anodeTimes
+  
 
 for filename in os.listdir(pmtDir):
-    #print(os.path.join(pmtDir, filename))       #prints the name of the file in the directory
-    
-    name = os.path.join(pmtDir, filename)
-    index = len(name) - 7
+    name = os.path.join(pmtDir, filename)                          
+    index = len(name) - 5
   
-    timeStampVal = int(name[index - 6]) * 600000 + int(name[index - 5]) * 60000 + int(name[index - 3]) * 10000 + int(name[index - 2]) * 1000 + int(name[index]) * 100 + int(name[index + 1]) * 10 + int(name[index + 2]) #taking the last three number in the timestamp (milliseconds) and adding it to anodeTimes
+    timeStampVal = int(name[index - 8]) * 600000 + int(name[index - 7]) * 60000 + int(name[index - 5]) * 10000 + int(name[index - 4]) * 1000 + int(name[index - 2]) * 100 + int(name[index - 1]) * 10 + int(name[index]) #converting the timestamp to milliseconds
 
 
-    pmtTimes.append(timeStampVal) 
-     
-    
-    #print('\n')
+    pmtTimes.append(timeStampVal)                                   #add timestamp value to pmtTimes
 
-   
+
+
+
+
+
+#The following section can be uncommented to display the timestamp values in anodeTimes and pmtTimes
+#This process slows down the program
+'''   
 print("-------------------- now displaying anodeTimes --------------") 
 
-for i in range(len(anodeTimes)):  
+for i in range(len(anodeTimes)):                                        #iterate through and print timestamp values of anodeTimes
     print(anodeTimes[i])
     print('\n')
 
 print("-------------------- now displaying pmtTimes --------------")
-
-for i in range(len(pmtTimes)):  
+ 
+for i in range(len(pmtTimes)):                                          #iterate through and print timestamp values of pmtTimes
     print(pmtTimes[i])
     print('\n')
+'''
+
+
+
+
 
 
 timeDifs = [] #difference between timestamps is stored here
 
 
-count = 0
+count = 1  #serves as an index for the time differences
 
 for a, p in zip(anodeTimes, pmtTimes): 
     timeDifs.append( abs(a - p) )
-    print(abs( a - p ), count)
+    #print(count, abs( a - p ))        #This can be uncommented to print the index then the time difference, however this slows down the program
     count = count + 1
 
-#also do standard deviation
-#make sure to include 
+print("\n")
 
 
+
+
+
+
+#calculate the average and standard deviation of the time differences
 print("The average time difference between the two oscilloscopes is: ")
 print(sum(timeDifs) / len(timeDifs))
 print("\n")
 print("the standard deviation is:")
 print(statistics.stdev(timeDifs))
 
-np.histogram(timeDifs, bins=20, range=None, normed=None, weights=None, density=None)   #these lines create a histogram
+#The following lines generate a histogram in a popup window
+np.histogram(timeDifs, bins=20, range=None, normed=None, weights=None, density=None)   
 plt.hist(timeDifs, bins ='auto')
 plt.title("time differences")
 plt.show()
+
+
 
 #pmt folder usually has one more acquisition than the anode folder. This can cause large discrepancies when 
 #multiple acquisitions runs are kept in the same folder as non-corresponding acquisitions are subtracted
